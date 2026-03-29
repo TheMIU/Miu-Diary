fc08d60c017ac82b5854eef2c4b0cf457ac39a4e
TheMIU <kasunmiuranga2000@gmail.com>
2026-03-29 21:30:56 +0530

    march 29

--- entries.js
+++ entries.js
@@ -7,8 +7,114 @@
 //  notes — string (optional, leave "" if none)
 //  links — array of URLs (optional, leave [] if none)
 // ============================================================
-const entries = [
 
+/*     {
+        path: "img/2026/03/2026_03_29_1.png",
+        date: "2026-03-29",
+        tags: [],
+        notes: "",
+        links: []
+}, */
+    
+const entries = [
+    {
+        path: "img/2026/03/2026_03_29_12.png",
+        date: "2026-03-29",
+        tags: ["Miusoft", "godot"],
+        notes: "",
+        links: []
+    },
+    {
+        path: "img/2026/03/2026_03_29_11.png",
+        date: "2026-03-29",
+        tags: ["gamedev", "GodotSensei"],
+        notes: "",
+        links: ["https://godot-sinhala.miusoftgames.com/"]
+    },
+    {
+        path: "img/2026/03/2026_03_29_10.png",
+        date: "2026-03-29",
+        tags: ["YooGameArt", "gamedev"],
+        notes: "",
+        links: ["website: https://assets.dasca.studio/"]
+    },
+    {
+        path: "img/2026/03/2026_03_29_9.png",
+        date: "2026-03-29",
+        tags: ["films", "batman", "memos"],
+        notes: "",
+        links: []
+    },
+    {
+        path: "img/2026/03/2026_03_29_8.png",
+        date: "2026-03-29",
+        tags: ["MiuPlays", "Games"],
+        notes: "",
+        links: ["https://www.youtube.com/@MiuPlayGames"]
+    },
+    {
+        path: "img/2026/03/2026_03_29_7.png",
+        date: "2026-03-29",
+        tags: ["godot", "GodotSensei"],
+        notes: "කලින් post එක,",
+        links: ["https://www.linkedin.com/posts/kasun-miuranga_godot-godotengine-gamedev-activity-7438094661111664640-FPS2?utm_source=social_share_send&utm_medium=member_desktop_web&rcm=ACoAADM6alUBTddY3HSzUytLibqK6lajxFZyPKM"]
+    },
+    {
+        path: "img/2026/03/2026_03_29_6.png",
+        date: "2026-03-29",
+        tags: ["blender", "trees", "particles"],
+        notes: "Tutorial link එක, timestamp: 3:46",
+        links: ["https://www.youtube.com/watch?v=3owlx-NI1io"]
+    },
+    {
+        path: "img/2026/03/2026_03_29_5.png",
+        date: "2026-03-29",
+        tags: ["blender", "trees", "particles"],
+        notes: "Tutorial link එක, මේකෙ අන්තිම ටිකේ ඇත්තෙ.",
+        links: ["https://www.youtube.com/watch?v=o54gVHUPdCU"]
+    },
+    {
+        path: "img/2026/03/2026_03_29_4.png",
+        date: "2026-03-29",
+        tags: ["optimize", "texturing", "tools"],
+        notes: "imagecompressor link එක,",
+        links: ["https://imagecompressor.11zon.com/en/image-compressor/compress-image-without-losing-quality"]
+    },
+    {
+        path: "img/2026/03/2026_03_29_3.png",
+        date: "2026-03-29",
+        tags: ["blender", "texturing" ],
+        notes: "",
+        links: []
+    },
+    {
+        path: "img/2026/03/2026_03_29_2.png",
+        date: "2026-03-29",
+        tags: ["blender", "trees"],
+        notes: "Tutorial link එක,",
+        links: ["https://www.youtube.com/watch?v=v4dvQBPCtBA"]
+    },
+    {
+        path: "img/2026/03/2026_03_29_1.png",
+        date: "2026-03-29",
+        tags: ["blender", "rigging"],
+        notes: "",
+        links: []
+    },
+    {
+        path: "img/2026/03/2026_03_28_5.png",
+        date: "2026-03-28",
+        tags: ["film", "batman", "M Entertainment"],
+        notes: "පහළින් තියෙන්නෙ shorts playlist එකේ link එක",
+        links: ["https://www.youtube.com/playlist?list=PLBFTR8otL-ZVHrO64UaeoBYgCn7Asruip"]
+    },
+    {
+        path: "img/2026/03/2026_03_28_4.png",
+        date: "2026-03-28",
+        tags: ["blender"],
+        notes: "",
+        links: []
+    },
     {
         path: "img/2026/03/2026_03_28_3.png",
         date: "2026-03-28",
@@ -16,7 +122,6 @@ const entries = [
         notes: "FB post link එකයි, මං හදපු website link එකයි පහළ ඇති.",
         links: ["https://web.facebook.com/share/p/18hmFDRLdZ/", "https://themiu.github.io/Indie-Game-The-Movie-Sinhala-Sub"]
     },
-
     {
         path: "img/2026/03/2026_03_28_2.png",
         date: "2026-03-28",
@@ -24,7 +129,6 @@ const entries = [
         notes: "",
         links: []
     },
-
     {
         path: "img/2026/03/2026_03_28_1.png",
         date: "2026-03-28",
